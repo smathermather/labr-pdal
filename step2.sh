2018ec5027384e8bda8e061ff651d81cf7bd7237
Stephen Mather <stephen@smathermather.com>
2014-09-13 12:32:39 -0700

    remove cores from make

--- step2.sh
+++ step2.sh
@@ -8,5 +8,5 @@ cd ..
 wget http://download.osgeo.org/postgis/source/postgis-2.1.3.tar.gz
 tar -xzf postgis-2.1.3.tar.gz
 cd postgis-2.1.3 && ./configure --with-sfcgal=/usr/local/bin/sfcgal-config
-make -j3 && sudo make install
+make && sudo make install
 cd ..
